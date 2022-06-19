7991f683aec2383459860dae20335b6a3dce4f3d
Dhruv Maheshwari <dhruv.maheshwari@perennialsys.com>
2022-06-19 20:46:55 +0530

    created new pages

---
+++ src/pages/Favorites.js
@@ -0,0 +1,7 @@
+export default function Favorites() {
+  return (
+    <div>
+      <h1>Hello To Favorites</h1>
+    </div>
+  );
+}
---
+++ src/pages/NewMeetUps.js
@@ -0,0 +1,7 @@
+export default function NewMeetUps() {
+  return (
+    <div>
+      <h1>Hello To New Meetups</h1>
+    </div>
+  );
+}
